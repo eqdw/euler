450d01bfb42ca6a36eca32458562fad117dc58e8
Tim Herd <rz@eqdw.net>
2013-06-13 18:15:48 -0700

    Tweak method signature

--- prob001.ex
+++ prob001.ex
@@ -1,6 +1,6 @@
 defmodule Prob001 do
   def multiple_sums(n) do
-    Enum.reduce 1..n, 0, fn
+    Enum.reduce 1..(n-1), 0, fn
       x, acc when (rem(x, 3) == 0 or rem(x, 5) == 0) -> acc + x
       _, acc -> acc
     end
